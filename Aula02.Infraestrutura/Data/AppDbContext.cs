a314e2845ed63e401ccfab97fcb59595af204685
AnaLe <analuizaalg110@gmail.com>
2025-04-29 16:02:34 -0300

    .

--- Aula02.Infraestrutura/Data/AppDbContext.cs
+++ Aula02.Infraestrutura/Data/AppDbContext.cs
@@ -6,7 +6,8 @@ namespace Aula02.Infraestrutura.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        
+
         public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
     }
 }
